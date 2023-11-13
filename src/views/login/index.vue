399ee7f5cc723160b6541fd57cbd2d7dde4c5d1e
litao <2532566074@qq.com>
2023-11-13 16:19:00 +0800

    密码框状态通用处理

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -16,9 +16,15 @@
         <span class="svg-container">
           <svg-icon icon="password" />
         </span>
-        <el-input placeholder="password" name="password" v-model="loginForm.password" />
+        <el-input
+          placeholder="password"
+          name="password"
+          v-model="loginForm.password"
+          ref="password"
+          :type="passwordType"
+        />
         <span class="show-pwd">
-          <svg-icon icon="eye" />
+          <svg-icon :icon="passwordType === 'password' ? 'eye' : 'eye-open'" @click="onChangePwdType" />
         </span>
       </el-form-item>
 
@@ -57,6 +63,16 @@ const loginRules = ref({
     }
   ]
 })
+
+// 处理密码框文本显示状态
+const passwordType = ref('password')
+const onChangePwdType = () => {
+  if (passwordType.value === 'password') {
+    passwordType.value = 'text'
+  } else {
+    passwordType.value = 'password'
+  }
+}
 </script>
 
 <style lang="scss" scoped>
