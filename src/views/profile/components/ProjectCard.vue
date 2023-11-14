463c31e6ca35111f7c38dd0c8d91602a7aa502b2
litao <2532566074@qq.com>
2023-11-14 12:04:45 +0800

    创建头像组件 Avatar

--- src/views/profile/components/ProjectCard.vue
+++ src/views/profile/components/ProjectCard.vue
@@ -1,7 +1,14 @@
 <template>
-  <div></div>
+  <div class="">
+    <my-avatar :image="$store.getters.userInfo.avatar" :height="'100px'" :width="'100px'" :hoverable="false">
+      <div>Hello</div>
+      {{ $store.getters.userInfo.title }}
+    </my-avatar>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import MyAvatar from '@/components/MyAvatar'
+</script>
 
 <style lang="scss" scoped></style>
