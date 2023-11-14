e62a671f72eb5dba169f4f37863893e4166f5671
litao <2532566074@qq.com>
2023-11-14 12:47:10 +0800

    全局属性处理时间展示问题

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -4,6 +4,7 @@
       <el-avatar :size="logoHeight" shape="square" src="https://i2.100024.xyz/2023/11/12/10j9cux.webp" />
       <h1 class="logo-title" v-if="$store.getters.sidebarOpened">super-admin</h1>
     </div>
+    <span style="color: #fff; margin-left: 20px">{{ $filters.dateFilter(now) }}</span>
     <el-scrollbar>
       <sidebar-menu></sidebar-menu>
     </el-scrollbar>
