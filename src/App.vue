bf335b836608d7027508ff9c280bfb3234bc3172
litao <2532566074@qq.com>
2023-11-14 14:54:36 +0800

    实现动态表格能力

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,32 @@ const store = useStore()
 generateNewStyle(store.getters.mainColor).then((newStyleText) => {
   writeNewStyle(newStyleText)
 })
+
+const debounce = (fn, delay) => {
+  let timer = null
+
+  return function () {
+    const context = this
+
+    const args = arguments
+
+    clearTimeout(timer)
+
+    timer = setTimeout(function () {
+      fn.apply(context, args)
+    }, delay)
+  }
+}
+
+const _ResizeObserver = window.ResizeObserver
+
+window.ResizeObserver = class ResizeObserver extends _ResizeObserver {
+  constructor(callback) {
+    callback = debounce(callback, 16)
+
+    super(callback)
+  }
+}
 </script>
 
 <style></style>
