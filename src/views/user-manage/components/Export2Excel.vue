1302a73bd3743afcdea1418f535713602b6272f6
litao <2532566074@qq.com>
2023-11-14 13:54:26 +0800

    导出前置业务处理

--- src/views/user-manage/components/Export2Excel.vue
+++ src/views/user-manage/components/Export2Excel.vue
@@ -1,16 +1,18 @@
 <template>
   <el-dialog :title="$t('msg.excel.title')" :model-value="modelValue" @close="closed" width="30%">
-    <el-input :placeholder="$t('msg.excel.placeholder')"></el-input>
+    <el-input :placeholder="请输入" v-model="excelName"></el-input>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="closed">{{ $t('msg.excel.close') }}</el-button>
-        <el-button type="primary" @click="onConfirm">{{ $t('msg.excel.confirm') }}</el-button>
+        <el-button type="primary" @click="onConfirm" :loading="loading">{{ $t('msg.excel.confirm') }}</el-button>
       </span>
     </template>
   </el-dialog>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { getUserManageList } from '@/api/system'
 defineProps({
   modelValue: {
     type: Boolean,
@@ -19,10 +21,18 @@ defineProps({
 })
 const emits = defineEmits(['update:modelValue'])
 
+const exportDefaultName = '用户信息表'
+const excelName = ref('')
+excelName.value = exportDefaultName
+
 /**
  * 导出按钮点击事件
  */
+const loading = ref(false)
 const onConfirm = async () => {
+  loading.value = true
+  const userList = (await getUserManageList()).rows
+  console.log(userList)
   closed()
 }
 
