65e20069e2dad6ef2584cd47046532790694f185
litao <2532566074@qq.com>
2023-11-14 15:58:53 +0800

    创建文章的基本结构

--- src/views/article-create/index.vue
+++ src/views/article-create/index.vue
@@ -1,7 +1,40 @@
 <template>
-  <div></div>
+  <div class="article-create">
+    <el-card>
+      <el-input
+        class="title-input"
+        :placeholder="$t('msg.article.titlePlaceholder')"
+        v-model="title"
+        maxlength="20"
+        clearable
+      >
+      </el-input>
+      <el-tabs v-model="activeName">
+        <el-tab-pane :label="$t('msg.article.markdown')" name="markdown">
+          <markdown></markdown>
+        </el-tab-pane>
+        <el-tab-pane :label="$t('msg.article.richText')" name="editor">
+          <editor></editor>
+        </el-tab-pane>
+      </el-tabs>
+    </el-card>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import Editor from './components/Editor.vue'
+import Markdown from './components/Markdown.vue'
+import { ref } from 'vue'
 
-<style lang="scss" scoped></style>
+const activeName = ref('markdown')
+const title = ref('')
+</script>
+
+<style lang="scss" scoped>
+.article-create {
+  margin-top: 30px;
+}
+.title-input {
+  margin-bottom: 20px;
+}
+</style>
